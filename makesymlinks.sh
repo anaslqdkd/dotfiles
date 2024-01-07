6d7168c2cdb6c96ce427e588048de051bbb4d0f7
Ana Ceban <ana.ceban@telecomnancy.eu>
2024-01-07 22:39:04 +0100

    commit dotfiles

---
+++ makesymlinks.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+######################################
+# .make.sh
+# Script to create symlinks from the home directory to dotfiles in ~/dotfiles
+
+######### Variables
+
+dir = ~/dotfiles
+olddir = ~/dotfiles_old
+files = "vimrc vim Xresources "
+
+#########
+
+# create dorfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $oldir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in the home directory"
+    ln -s $dir/$file ~/.$file
+done 
